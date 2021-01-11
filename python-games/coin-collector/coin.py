4763dff72c236d0ad1b45cacb17e740b86ba1761
Ishaan Kale <ishaannk@outlook.com>
2021-01-11 07:59:58 +0530

    Added 2 statments

--- python-games/coin-collector/coin.py
+++ python-games/coin-collector/coin.py
@@ -17,7 +17,8 @@ def draw():
     screen.draw.text("Score: " + str(score), color="black", topleft=(10, 10))
 
 def place_coin():
-    pass
+    coin.x = randint(20, (WIDTH - 20))
+    coin.y = randint(20, (HEIGHT - 20))
 
 def time_up():
     pass
