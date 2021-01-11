fb76b3cd59b4b1952c0fef0e905599150ba6a00d
Ishaan Kale <ishaannk@outlook.com>
2021-01-11 11:13:29 +0530

    Drew the actors and drew the lines

--- python-games/follow-the-numbers/numbers.py
+++ python-games/follow-the-numbers/numbers.py
@@ -9,7 +9,19 @@ lines = []
 next_dot = 0
 
 for dot in range(0, 10):
-    actor = Actor("dot")
+    actor = Actor('dot')
     actor.pos = randint(20, WIDTH - 20),
     randint(20, HEIGHT - 20)
     dots.append(actor)
+
+def draw():
+    screen.fill('black')
+    number = 1
+    for dot in dots:
+        screen.draw.text(str(number)), \
+            (dot.pos[0], dot.pos[1] + 12))
+
+        dot.draw()
+        number = number + 1
+    for line in lines:
+        screen.draw.lines(line[0], line[1], (65, 105, 225))
