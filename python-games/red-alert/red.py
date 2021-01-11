d4bce04531f94b85398b8b96d25223854ca3c82f
Ishaan Kale <ishaannk@outlook.com>
2021-01-11 12:55:36 +0530

    Delared the constants

--- python-games/red-alert/red.py
+++ python-games/red-alert/red.py
@@ -1,1 +1,10 @@
 import random
+
+FONT_COLOR = (255, 255, 255)
+WIDTH = 800
+HEIGHT = 600
+CENTER_X = WIDTH / 2
+CENTER_Y = HEIGHT / 2
+FINAL_LEVEL = 6
+START_SPEED = 10
+COLOURS = ['green', 'blue']
