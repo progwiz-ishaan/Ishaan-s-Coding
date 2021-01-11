cfe0775487afd5e7f50563bd684850dd660a70a7
Ishaan Kale <ishaannk@outlook.com>
2021-01-11 08:23:07 +0530

    Added 5 if statments and a variable

--- python-games/coin-collector/coin.py
+++ python-games/coin-collector/coin.py
@@ -16,14 +16,35 @@ def draw():
     coin.draw()
     screen.draw.text("Score: " + str(score), color="black", topleft=(10, 10))
 
+    if game_over:
+        screen.fill("pink")
+        screen.draw.text("Final Score: " + str(score), topleft=(10, 10), fontsize=60)
+
 def place_coin():
     coin.x = randint(20, (WIDTH - 20))
     coin.y = randint(20, (HEIGHT - 20))
 
 def time_up():
-    pass
+    global game_over
+    game_over = True
 
 def update():
-    pass
+    global score
 
+    if keyboard.left:
+        fox.x = fox.x - 2
+    elif keyboard.right:
+        fox.x = fox.x + 2
+    elif keyboard.up:
+        fox.y = fox.y - 2
+    elif keyboard.down:
+        fox.y = fox.y + 2
+
+    coin_collected = fox.colliderect(coin)
+
+    if coin_collected:
+        score = score + 10
+        place_coin()
+
+clock.schedule(time_up, 7.0)
 place_coin()
