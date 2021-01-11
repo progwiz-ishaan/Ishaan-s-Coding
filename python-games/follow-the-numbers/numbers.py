5fa4b635952acdc541ff99c36f9a18d7efd3abcc
Ishaan Kale <ishaannk@outlook.com>
2021-01-11 10:59:29 +0530

    Set up the characters

--- python-games/follow-the-numbers/numbers.py
+++ python-games/follow-the-numbers/numbers.py
@@ -7,3 +7,9 @@ dots = []
 lines = []
 
 next_dot = 0
+
+for dot in range(0, 10):
+    actor = Actor("dot")
+    actor.pos = randint(20, WIDTH - 20),
+    randint(20, HEIGHT - 20)
+    dots.append(actor)
