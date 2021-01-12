c076d05d0915e5097dec3c41d8f03285fb9aad8e
Ishaan Kale <ishaannk@outlook.com>
2021-01-12 15:34:23 +0530

    Created 5 functions

--- python-games/big-quiz/quiz.py
+++ python-games/big-quiz/quiz.py
@@ -1,2 +1,17 @@
 WIDTH = 1208
 HEIGHT = 720
+
+def draw():
+    pass
+
+def game_over():
+    pass
+
+def correct_answer():
+    pass
+
+def on_mouse_down():
+    pass
+
+def update_time_left():
+    pass
