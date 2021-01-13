083a0271a7ccdeb04e61cebcf8c3edbaf7c52b22
Ishaan Kale <ishaannk@outlook.com>
2021-01-13 17:42:33 +0530

    Created 4 variables and added a function call

--- python-games/balloon-flight/balloon.py
+++ python-games/balloon-flight/balloon.py
@@ -3,3 +3,17 @@ from random import randint
 
 WIDTH = 800
 HEIGHT = 600
+
+balloon = Actor('balloon')
+balloon.pos = 400, 300
+
+bird = Actor('bird-up')
+bird.pos = randint(800, 1600), randint(10, 200)
+
+house = Actor('house')
+house.pos = randint(800, 1600), 460
+
+tree = Actor('tree')
+tree.pos = randint(800, 1600), 450
+
+pgzrun.go()
