b2ccf325bfb97b64fd2d1da74a3e43bc27f219ee
Ishaan Kale <ishaannk@outlook.com>
2021-01-15 13:03:33 +0530

    Created  14 variables

--- python-games/dance-challenge/dance.py
+++ python-games/dance-challenge/dance.py
@@ -1,4 +1,22 @@
 import pgzrun
 from random import randint
 
+WIDTH = 800
+HEIGHT = 600
+CENTER_X = WIDTH / 2
+CENTER_Y = HEIGHT / 2
+
+move_list = []
+display_list = []
+
+score = 0
+current_move = 0
+count = 4
+dance_length = 4
+
+say_dance = False
+show_countdown = True
+moves_complete = False
+game_over = False
+
 pgzrun.go()
