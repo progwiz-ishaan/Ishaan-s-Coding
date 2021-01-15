925ac2ee54d3b4ffc60a4a6fd560a54035899722
Ishaan Kale <ishaannk@outlook.com>
2021-01-15 19:38:29 +0530

    Did nothing

--- python-games/dance-challenge/dance.py
+++ python-games/dance-challenge/dance.py
@@ -45,28 +45,114 @@ def draw():
         screen.draw.text('Score: ' +
                          str(score), color='black',
                          topleft=(10, 10))
+        if say_dance:
+            screen.draw.text('Dance!!!', color='black',
+            topleft=(CENTER_X - 65, 150), fontsize=60)
+        if show_countdown:
+            screen.draw.text(str(count), color='black',
+            topleft=(CENTER_X - 8, 150), fontsize=60)
+    else:
+        screen.clear()
+        screen.blit('stage', (0, 0))
+        screen.draw.text('Score: ', color='black' + str(score),
+                         topleft=(10, 10))
+        screen.draw.text('GAME OVER!', color='black', topleft=(CENTER_X - 130, 220), fontsize=60)
     return
 
 def reset_dancer():
-    pass
+    global game_over
+    if not game_over:
+        dancer.image = 'dancer-start'
+        up.image = 'up'
+        left.image = 'left'
+        right.image = 'right'
+        down.image = 'down'
+    return
 
 def update_dancer(move):
-    pass
+    global game_over
+    if not game_over:
+        if move == 0:
+            up.image = 'up-lit'
+            dancer.image = 'dancer-up'
+            clock.schedule(reset_dancer, 0.5)
+        elif move == 1:
+            right.image = 'right-lit'
+            dancer.image = 'dancer-right'
+            clock.schedule(reset_dancer, 0.5)
+        elif move == 2:
+            down.image = 'down-lit'
+            dancer.image = 'dancer-down'
+            clock.schedule(reset_dancer, 0.5)
+        else:
+            left.image = 'left-lit'
+            dancer.image = 'dancer-left'
+            clock.schedule(reset_dancer, 0.5)
 
 def display_moves():
-    pass
+    global move_list, display_list, dance_length, say_dance, show_countdown, current_move
+    if display_list:
+        this_move = display_list[0]
+        display_list = display_list[1:]
+        if this_move == 0:
+            update_dancer(0)
+            clock.schedule(display_moves, 1)
+        elif this_move == 1:
+            update_dancer(1)
+            clock.schedule(display_moves, 1)
+        elif this_move == 2:
+            update_dancer(2)
+            clock.schedule(display_moves, 1)
+        else:
+            update_dancer(3)
+            clock.schedule(display_moves, 1)
+    else:
+        say_dance = True
+        show_countdown = False
+    return
 
 def genrate_moves():
-    pass
+    global move_list, dance_length, count, show_countdown, say_dance
+    count = 4
+    move_list = []
+    say_dance = False
+    for move in range(0, dance_length):
+        rand_move = randint(0, 3)
+        move_list.append(rand_move)
+        display_list.append(rand_move)
+    show_countdown = True
+    countdown()
+    return
 
 def countdown():
-    pass
+    global count, game_over, show_countdown
+    if count > 1:
+        count -= 1
+        clock.schedule(countdown, 1)
+    else:
+        show_countdown = False
+        display_moves()
 
 def next_move():
-    pass
+    global dance_length, current_move, moves_complete
+    if current_move < dance_length - 1:
+        current_move += 1
+    else:
+        moves_complete = True
+    return
 
 def on_key_up(key):
-    pass
+    global score, game_over, move_list, current_move
+    if key == keys.UP:
+        update_dancer(0)
+    elif key == keys.DOWN:
+        update_dancer(2)
+    elif key == keys.RIGHT:
+        update_dancer(1)
+    elif key == keys.LEFT:
+        update_dancer(3)
+
+genrate_moves()
 
 def update():
     pass
