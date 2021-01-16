005f60cf010665f30e76f032e69b57ebe72c1aa8
Ishaan Kale <ishaannk@outlook.com>
2021-01-16 15:53:00 +0530

    Created 6 variables

--- python-games/happy-garden/garden.py
+++ python-games/happy-garden/garden.py
@@ -6,3 +6,11 @@ WIDTH = 800
 HEIGHT = 600
 CENTER_X = WIDTH / 2
 CENTER_Y = HEIGHT / 2
+
+game_over = False
+finalised = False
+garden_happy = True
+fangflower_collision = False
+
+time_elasped = 0
+start_time = time.time()
