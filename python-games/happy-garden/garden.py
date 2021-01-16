826da1b91de1f68466c74b3992b6d8765c76c617
Ishaan Kale <ishaannk@outlook.com>
2021-01-16 16:08:03 +0530

    Defined a function

--- python-games/happy-garden/garden.py
+++ python-games/happy-garden/garden.py
@@ -24,3 +24,20 @@ fangflower_list = []
 
 fangflower_vy_list = []
 fangflower_vx_list = []
+
+def draw():
+    global game_over, time_elasped, finalised
+    if not game_over:
+        screen.clear()
+        screen.blit('garden', (0, 0))
+        cow.draw()
+        for flower in flower_list:
+            flower.draw()
+        for fangflower in fangflower_list:
+            fangflower.draw()
+        time_elasped = int(time.time() - start_time)
+        screen.draw.text(
+            'Garden happy for:' + 
+            str(time_elasped) + ' seconds',
+            topleft=(10, 10), color='black'
+        )
