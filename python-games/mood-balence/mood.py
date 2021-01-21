5e95d96848cab7d8434030281a471ddccdde33ba
Ishaan Kale <ishaannk@outlook.com>
2021-01-21 12:18:56 +0530

    Defined a function

--- python-games/mood-balence/mood.py
+++ python-games/mood-balence/mood.py
@@ -6,10 +6,22 @@ HEIGHT = 400
 CENTER_X = WIDTH / 2
 CENTER_Y = WIDTH / 2
 CENTER = (CENTER_X, CENTER_Y)
-HAPPY_FACES_NEEDED = 14
+HAPPY_FACES_NEEDED = randint(2, 5)
 
 game_over = False
 game_complete = False
-happy_face_count = 0
+face_count = 0
+
+face = Actor('sad-face', pos=CENTER)
+
+def draw():
+    screen.clear()
+    face.draw()
+    screen.draw.text(
+        'Face.no: ' + 
+        str(face_count),
+        color=(0, 0, 0),
+        topleft=(10, 10)
+    )
 
 pgzrun.go()
