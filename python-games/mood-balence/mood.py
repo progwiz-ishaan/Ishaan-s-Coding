c63f2f9fb99bf43c0e38ce3de5e586f3718c63d4
Ishaan Kale <ishaannk@outlook.com>
2021-01-21 12:01:25 +0530

    Renamed a folder

---
+++ python-games/mood-balence/mood.py
@@ -0,0 +1,15 @@
+import pgzrun
+
+WIDTH = 400
+HEIGHT = 400
+CENTER_X = WIDTH / 2
+CENTER_Y = WIDTH / 2
+CENTER = (CENTER_X, CENTER_Y)
+HAPPY_FACES_NEEDED = 14
+
+game_over = False
+game_complete = False
+happy_face_count = 0
+
+
+pgzrun.go()
