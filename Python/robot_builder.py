3358d43a1132042a6bfc485bb6f1952cf446873e
Ishaan Kale <ishaannk@outlook.com>
2021-02-13 10:03:49 +0530

    s

--- Python/robot_builder.py
+++ Python/robot_builder.py
@@ -1,24 +1,67 @@
-import turtle
+import turtle as t
 
 def rectangle(horizontal, vertical, color):
-    turtle.pendown()
-    turtle.pensize(1)
-    turtle.color(color)
-    turtle.begin_fill()
+    t.pendown()
+    t.pensize(1)
+    t.color(color)
+    t.begin_fill()
     for counter in range(1, 3):
-        turtle.forward(horizontal)
-        turtle.right(90)
-        turtle.forward(vertical)
-        turtle.right(90)
-    turtle.end_fill()
-    turtle.penup()
+        t.forward(horizontal)
+        t.right(90)
+        t.forward(vertical)
+        t.right(90)
+    t.end_fill()
+    t.penup()
 
-turtle.penup()
-turtle.speed('slow')
-turtle.bgcolor('Doger blue')
+t.penup()
+t.speed('slow')
+t.bgcolor('Doger blue')
 
 # feet
-turtle.goto(-100, -150)
+t.goto(-100, -150)
 rectangle(50, 20, 'blue')
-turtle.goto(-30, -150)
+t.goto(-30, -150)
 rectangle(50, 20, 'blue')
+
+# legs
+t.goto(-25, -50)
+rectangle(15, 100, 'grey')
+t.goto(-55, 50)
+rectangle(-15, 100, 'grey')
+
+# body
+t.goto(-90, 100)
+rectangle(100, 150, 'red')
+
+# arms
+t.goto(-150, 70)
+rectangle(60, 15, 'grey')
+t.goto(-150, 110)
+rectangle(15, 40, 'grey')
+
+t.goto(10, 70)
+rectangle(60, 15, 'grey')
+t.goto(-155, 110)
+rectangle(15, 40, 'grey')
+
+# neck
+t.goto(-50, 120)
+rectangle(15, 20, 'grey')
+
+# head
+t.goto(-85, 170)
+rectangle(80, 50, 'red')
+
+# eyes
+t.goto(-60, 160)
+rectangle(30, 10, 'white')
+t.goto(-55, 155)
+rectangle(5, 5, 'black')
+t.goto(-40, 155)
+rectangle(5, 5, 'black')
+
+# mouth
+t.goto(-65, 135)
+rectangle(60, 15, 'grey')
+
+t.hideturtle()
