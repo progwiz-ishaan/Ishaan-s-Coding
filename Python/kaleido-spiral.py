878113ff006a4efca9899685da330adeeac84896
Ishaan Kale <ishaannk@outlook.com>
2021-02-13 17:12:07 +0530

    Defined a function

--- Python/kaleido-spiral.py
+++ Python/kaleido-spiral.py
@@ -1,10 +1,13 @@
 import turtle as t
 
+def draw_circle(size):
+    t.pencolor('red')
+    t.circle(size)
+    draw_circle(size)
+
 t.bgcolor('black')
 t.speed('fast')
 t.pensize(4)
-
-t.pencolor('red')
-t.circle(30)
+draw_circle(30)
 
 keep_going_var = input()
