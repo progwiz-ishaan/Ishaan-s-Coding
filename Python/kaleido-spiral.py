402327a4a6ae45b1b704af952b04d1b4ce85bf29
Ishaan Kale <ishaannk@outlook.com>
2021-02-13 17:18:07 +0530

    Added new parameters

--- Python/kaleido-spiral.py
+++ Python/kaleido-spiral.py
@@ -1,13 +1,18 @@
 import turtle as t
+from itertools import cycle
 
-def draw_circle(size):
-    t.pencolor('red')
+colors = cycle(['red', 'orange', 'blue', 'purple', 'yellow', 'green'])
+
+def draw_circle(size, angle, shift):
+    t.pencolor(next(colors))
     t.circle(size)
-    draw_circle(size)
+    t.right(angle)
+    t.forward(shift)
+    draw_circle(size + 5, angle + 1, shift + 1)
 
 t.bgcolor('black')
 t.speed('fast')
 t.pensize(4)
-draw_circle(30)
+draw_circle(30, 0, 1)
 
 keep_going_var = input()
