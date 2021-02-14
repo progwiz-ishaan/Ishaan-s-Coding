344d2af444d244005b3a49af6b7e2e5a45fedc00
Ishaan Kale <ishaannk@outlook.com>
2021-02-14 13:57:00 +0530

    Defined 2 functions

--- Python/rainbow.py
+++ Python/rainbow.py
@@ -1,2 +1,22 @@
 import random as r
 import turtle as t
+
+def get_line_length():
+    choice = input ('Enter line length (long, medium, short): ')
+    if choice == 'long':
+        line_length = 250
+    elif choice == 'meidum':
+        line_length = 200
+    else:
+        line_length = 100
+    return line_length
+
+def get_line_width():
+    choice = input ('Enter line width (superthick, thick, thin): ')
+    if choice == 'superthick':
+        line_width = 40
+    elif choice == 'thick':
+        line_width = 25
+    else:
+        line_width = 10
+    return line_width
