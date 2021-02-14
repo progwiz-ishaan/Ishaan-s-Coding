effea2e3a885aacdd1e972fe23089d9b6d5af8b7
Ishaan Kale <ishaannk@outlook.com>
2021-02-14 09:43:50 +0530

    Changed a value of an Integar

--- Python/starry_night.py
+++ Python/starry_night.py
@@ -1,7 +1,7 @@
 import turtle as t
 
 size = 300
-points = 5
+points = 11
 angle = 180 - (180 / points)
 
 t.color('yellow')
@@ -9,7 +9,7 @@ t.begin_fill()
 for i in range(points):
     t.forward(size)
     t.right(angle)
-    
+
 t.end_fill()
 
 keep_going_var = input()
