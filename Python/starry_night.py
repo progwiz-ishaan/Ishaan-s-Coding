4343c1678b3422f0a1b010e0e4198d473975e835
Ishaan Kale <ishaannk@outlook.com>
2021-02-14 09:51:09 +0530

    Defined a function

--- Python/starry_night.py
+++ Python/starry_night.py
@@ -1,15 +1,19 @@
 import turtle as t
 
-size = 300
-points = 11
-angle = 180 - (180 / points)
+def draw_star(points, size, col, x, y):
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
+    angle = 180 - (180 / points)
+    t.color(col)
+    t.begin_fill()
+    for i in range(points):
+        t.forward(size)
+        t.right(angle)
+    t.end_fill()
 
-t.color('yellow')
-t.begin_fill()
-for i in range(points):
-    t.forward(size)
-    t.right(angle)
-
-t.end_fill()
+# Main code
+t.Screen().bgcolor('dark blue')
+draw_star(5, 50, 'dodger blue', 0, 0)
 
 keep_going_var = input()
