2caef38cf30804dd53f25cf98c0500d83858de12
Ishaan Kale <ishaannk@outlook.com>
2021-02-14 09:41:19 +0530

    Added 3 function calls

--- Python/starry_night.py
+++ Python/starry_night.py
@@ -4,8 +4,11 @@ size = 300
 points = 5
 angle = 180 - (180 / points)
 
+t.color('yellow')
+t.begin_fill()
 for i in range(points):
     t.forward(size)
     t.right(angle)
+t.end_fill()
 
 keep_going_var = input()
