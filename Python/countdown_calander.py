77b92efc3dd61328792cd2c57ee570b4e2fd3fc6
Ishaan Kale <ishaannk@outlook.com>
2021-02-15 08:22:07 +0530

    Creted 2 Variables

--- Python/countdown_calander.py
+++ Python/countdown_calander.py
@@ -1,2 +1,8 @@
 from tkinter import Tk, Canvas
 from datetime import date, datetime
+
+root = Tk()
+c = Canvas(root, width=800, height=800, bg='black')
+c.pack()
+c.create_text(100, 50, anchor='w', fill='orange',\
+    font='Arial 28 bold underline', text='My Countdown Calender')
