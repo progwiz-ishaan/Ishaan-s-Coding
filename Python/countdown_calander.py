64681d772c94dbeb6e864ed7ce34ec0c16175312
Ishaan Kale <ishaannk@outlook.com>
2021-02-15 08:23:15 +0530

    Added a Function call

--- Python/countdown_calander.py
+++ Python/countdown_calander.py
@@ -6,3 +6,5 @@ c = Canvas(root, width=800, height=800, bg='black')
 c.pack()
 c.create_text(100, 50, anchor='w', fill='orange',\
     font='Arial 28 bold underline', text='My Countdown Calender')
+
+root.mainloop()
