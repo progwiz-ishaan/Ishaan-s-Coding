fb00be0312a3a05c134b5380e53face1e1d45949
Ishaan Kale <ishaannk@outlook.com>
2021-02-17 09:14:38 +0530

    d

--- Python/secret_messages.py
+++ Python/secret_messages.py
@@ -7,6 +7,23 @@ def get_even_letters(message):
         if is_even(counter):
             even_letters.append(message[counter])
     return even_letters
+def get_odd_letters(message):
+    odd_letters = []
+    for counter in range(0, len(message)):
+        if not is_even(counter):
+            odd_letters.append(message[counter])
+    return odd_letters
+def swap_letters(message):
+    letter_list = []
+    if not is_even(message):
+        message = message + 'x'
+    even_letters = get_even_letters(message)
+    odd_letters = get_odd_letters(message)
+    for counter in range(0, int(len(message)/2)):
+        letter_list.append(odd_letters[counter])
+        letter_list.append(even_letters[counter])
+    new_message = ''.join(letter_list)
+    return new_message
 def get_task():
     task = simpledialog.askstring('Task', 'Do you want to encrypt or decrypt?')
     return task
@@ -20,7 +37,9 @@ while True:
     task = get_task()
     if task == 'encrypt':
         message = get_message()
-        messagebox.showinfo('Message to encrypt is:', message)
+        encrypted = swap_letters(message)
+        messagebox.showinfo('Ciphertext of the message is:', encrypted)
     if task == 'decrypt':
         message = get_message()
-        messagebox.showinfo('Message to decrypt is:', message)
+        decrypted = swap_letters(message)
+        messagebox.showinfo('Plaintext of the message is:', decrypted)
