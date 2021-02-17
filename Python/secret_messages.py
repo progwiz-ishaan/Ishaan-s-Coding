70ae238730695a1a602cbfa6283b1dc6b5532e43
Ishaan Kale <ishaannk@outlook.com>
2021-02-17 08:43:51 +0530

    Added a while loop

--- Python/secret_messages.py
+++ Python/secret_messages.py
@@ -1,1 +1,18 @@
 from tkinter import simpledialog, messagebox, Tk
+def get_task():
+    task = simpledialog.askstring('Task', 'Do you want to encrypt or decrypt?')
+    return task
+def get_message():
+    message = simpledialog.askstring('Message', 'Enter the secret message: ')
+    return message
+
+root = Tk()
+
+while True:
+    task = get_task()
+    if task == 'encrypt':
+        message = get_message()
+        messagebox.showinfo('Message to encrypt is:', message)
+    if task == 'decrypt':
+        message = get_message()
+        messagebox.showinfo('Message to decrypt is:', message)
