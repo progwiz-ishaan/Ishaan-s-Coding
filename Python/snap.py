3f48f50848daafd10dd06ee402cfa433f27b2ada
Ishaan Kale <ishaannk@outlook.com>
2021-02-20 14:38:54 +0530

    Created 4 circles

--- Python/snap.py
+++ Python/snap.py
@@ -4,5 +4,18 @@ from tkinter import Tk, Canvas, HIDDEN, NORMAL
 
 root = Tk()
 root.title('Snap')
+c = Canvas(root, width=400, height=400)
+
+shapes = []
+
+circle = c.create_oval(35, 20, 365, 350, outline='black', fill='black', state=HIDDEN)
+shapes.append(circle)
+circle = c.create_oval(35, 20, 365, 350, outline='red', fill='red', state=HIDDEN)
+shapes.append(circle)
+circle = c.create_oval(35, 20, 365, 350, outline='green', fill='green', state=HIDDEN)
+shapes.append(circle)
+circle = c.create_oval(35, 20, 365, 350, outline='blue', fill='blue', state=HIDDEN)
+shapes.append(circle)
+c.pack()
 
 root.mainloop()
