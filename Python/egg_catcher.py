7d15057beb3610e4a5547df050559e455223eff9
Ishaan Kale <ishaannk@outlook.com>
2021-02-21 16:33:45 +0530

    Defined 4 functions

--- Python/egg_catcher.py
+++ Python/egg_catcher.py
@@ -19,4 +19,55 @@ egg_speed = 500
 egg_interval = 4000
 diffulty_factor = 0.95
 
+catcher_colour = 'blue'
+catcher_width = 100
+catcher_height = 100
+catcher_start_x = canvas_width / 2 - catcher_width / 2
+catcher_start_y = canvas_height - catcher_height - 20
+catcher_start_x2 = catcher_start_x + catcher_width
+catcher_start_y2 = catcher_height + catcher_start_y
+
+catcher = c.create_arc(catcher_start_x, catcher_start_y, catcher_start_x2, catcher_start_y2, start=200, \
+    extent=140, style='arc', outline=catcher_colour, width=3)
+
+game_font = font.nametofont('TkFixedFont')
+game_font.config(size=18)
+
+score = 0
+score_text = c.create_text(10, 10, anchor='nw', font=game_font, fill='darkblue', text='Score: ' + str(score))
+
+lives_remaining = 3
+lives_text = c.create_text(10, 10, anchor='nw', font=game_font, fill='darkblue', text='Lives: ' + \
+    str(lives_remaining)
+
+eggs = []
+
+def create_egg():
+    x = randrange(10, 740)
+    y = 40
+    new_egg = c.create_oval(x, y, x + egg_width, y + egg_height, fill=next(colour_cycle), width=0)
+    eggs.append(new_egg)
+    root.after(egg_interval, create_egg)
+
+def move_eggs():
+    for egg in eggs:
+        (egg_x, egg_y, egg_x2, egg_y2) = c.coords(egg)
+        c.move(egg, 0, 10)
+        if egg_y2 > canvas_height:
+            egg_droped(egg)
+    root.after(egg_speed, move_eggs)
+
+def egg_droped(egg):
+    eggs.remove(egg)
+    c.delete(egg)
+    lose_a_life()
+    if lives_remaining == 0:
+        messagebox.showinfo('Game Over!', 'Final Score: ' + str(score))
+        root.destroy()
+
+def lose_a_life():
+    global lives_remaining
+    lives_remaining -= 1
+    c.itemconfigure(lives_text, text='Lives: ' + str (lives_remaining))
+
 root.mainloop()
