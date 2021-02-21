40f4f0e042a638dedad70fbcb8dcdf17c5438762
Ishaan Kale <ishaannk@outlook.com>
2021-02-21 15:26:08 +0530

    s

--- Python/egg_catcher.py
+++ Python/egg_catcher.py
@@ -10,3 +10,5 @@ c = Canvas(root, width=canvas_width, height=canvas_height, bg='deep sky blue')
 c.create_rectangle(-5, canvas_height - 100, canvas_width + 5, canvas_height + 5, fill='sea green', width=0)
 c.create_oval(-80, -80, 120, 120, fill='orange', width=0)
 c.pack()
+
+root.mainloop()
