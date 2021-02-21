19ec804e9119fdde5632afb5b69214ba784ebffd
Ishaan Kale <ishaannk@outlook.com>
2021-02-21 16:02:58 +0530

    Set up the eggs

--- Python/egg_catcher.py
+++ Python/egg_catcher.py
@@ -11,4 +11,12 @@ c.create_rectangle(-5, canvas_height - 100, canvas_width + 5, canvas_height + 5,
 c.create_oval(-80, -80, 120, 120, fill='orange', width=0)
 c.pack()
 
+colour_cycle = cycle(['light blue', 'light cyan', 'light pink', 'light yellow', 'light green'])
+egg_width = 45
+egg_height = 45
+egg_score = 10
+egg_speed = 500
+egg_interval = 4000
+diffulty_factor = 0.95
+
 root.mainloop()
