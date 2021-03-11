85c7423a4fbf45f343a5c1c79646d225cb1428ac
Ishaan Kale <ishaannk@outlook.com>
2021-03-11 14:24:58 +0530

    Deleting and moving to new repo

--- Python-Crash-cource/pizza.py
+++
@@ -1,9 +0,0 @@
-prompt = '\nEnter the topping you would like on your pizza.'
-prompt += '\nEnter \'quit\' if you are done: '
-
-topping = ''
-while topping != 'quit':
-    topping = input(prompt)
-
-    if topping != 'quit':
-        print(f'{topping.title()} added on your pizza!')
