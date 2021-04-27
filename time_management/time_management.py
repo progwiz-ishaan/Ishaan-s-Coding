da0d30ac9fc08a405374cca5c5735fcf3235874d
Ishaan Kale <ishaannk@outlook.com>
2021-04-27 18:40:05 +0530

    s

--- time_management/time_management.py
+++ time_management/time_management.py
@@ -4,7 +4,12 @@ from tkinter.messagebox import showinfo
 from datetime import timedelta, datetime
 from task import Task
 
-buttons = []
+# Functions
+def open_task_window(task_type):
+    """Opens a new window based on the task type."""
+    pass
+
+def add_task()
 
 # Set up the window.
 window = Tk()
@@ -12,16 +17,24 @@ window.title("Time Management for Deepti")
 window.configure(bg='black', width=400, height=400)
 
 # Set up the widgets.
-welcome = Label(window, text='Welcome back, Deepti!!', width=20, height=5, bg='black', fg='orange', font=("Cursive", 30))
+welcome = Label(window, text='Welcome back, Deepti!!', width=20, height=5, \
+bg='black', fg='orange', font=("Cursive", 30))
 welcome.pack()
 
-u1i1 = Button(window, text='More Urgent, More Impotant', fg='orange', bg='black')
+u1i1 = Button(window, text='More Urgent, More Impotant', fg='orange', \
+bg='black')
 u1i1.pack()
 
-u1i0 = Button(window, text="More Urgent, Less Impotant", fg='orange', bg='black')
+u1i0 = Button(window, text="More Urgent, Less Impotant", fg='orange', \
+bg='black')
 u1i0.pack()
 
-u0i1 = Button(window, text='Less Urgent, More Impotant', fg='orange', bg='black')
+u0i1 = Button(window, text='Less Urgent, More Impotant', fg='orange', \
+bg='black')
 u0i1.pack()
+
+u0i0 = Button(window, text="Less Urgent, Less Impotant", fg='orange', \
+bg='black')
+u0i0.pack()
 
 window.mainloop()
