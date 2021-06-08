410b7b8905afa4d21309135399bb84421a52ad32
Ishaan Kale <ishaannk@outlook.com>
2021-06-08 08:36:15 +0530

    s

---
+++ robot/robot.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+import json
+class Robot:
+    """A class to repersent a robot."""
+
+    def __init__(self, robot_dict):
+        """Initialize attributes of a robot."""
+        self.name = robot_dict['name']
+        self.date_created = robot_dict['date_created']
+        self.owner = robot_dict['owner']
+        self.password = robot_dict['password']
+
+def set_up_robot():
+    """Set up a new robot."""
+    print("Enter 'q' in any of the inputs(leaving the password) if you don't want to create a robot.")
+    # Get information about the robot.
+    name = input("Robot name: ")
+    if name == 'q':
+        return
+    owner = input("Robot owner name: ")
+    if owner == 'q':
+        return
+    password = input(f"Password for accessing robot(tip: {name}123): ")
+    date_created = str(datetime.now())
+    # Create a dict about the robot and store it in a file.
+    robot_dict = {
+        'name': name,
+        'owner': owner,
+        'date_created': date_created,
+        'password': password,
+    }
+    filename = '/home/ishaan/Documents/Ishaan\'s-Coding/robot/robots.json'
+    with open(filename, 'a') as f:
+        f.write("\n")
+        json.dump(robot_dict, f, indent=4)
+    print("Robot created sucessfully!")
+
+set_up_robot()
